fd79b255fe3a2b10a29ebdc4a918d858837d45a9
dohan <ldh5887@gmail.com>
2026-03-13 12:35:33 +0900

    thinq-parent-backend 틀 생성

---
+++ thinq-parent-backend/src/test/java/com/example/thinq_parent/ThinqParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thinq_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinqParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
